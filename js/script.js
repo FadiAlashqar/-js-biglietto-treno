8b643a68ead679817fb230949c3ec93af3ee3a4b
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-17 23:13:29 +0100

    [UPD] minors discount

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ let age = parseInt(prompt("inserisci l'età"));
 let price = km * 0.21;
 let finalPrice;
 
+if (age <= 18) {
+    finalPrice = price - (price * 20 / 100);
+}
+
+
+console.log(`il prezzo è di ${finalPrice}`)
