6623bd27c2609344e5d9ea94db9ad9d886fcc438
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-18 20:27:04 +0100

    [UPD] exercise corrected

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@ let km = parseInt(prompt("inserisci km da percorrere"));
 let age = parseInt(prompt("inserisci l'età"));
 let price = km * 0.21;
 let finalPrice;
+let message;
 
 if (age <= 18) {
     finalPrice = price - (price * 20 / 100);
+    message = `hai diritto ad uno sconto del 20%. Il prezzo del biglietto è di: ${finalPrice.toFixed(2)}`
+    
 }
 else if (age >= 65) {
     finalPrice = price - (price * 40 / 100);
+    message = `hai diritto ad uno sconto del 40%. Il prezzo del biglietto è di: ${finalPrice.toFixed(2)}`
 }
 else{
     finalPrice = price;
 }
-console.log(`il prezzo è di ${finalPrice}`)
+console.log(message)
